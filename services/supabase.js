e42e7f25232bce9899918baa50910961ac57f1da
José Olinda <joseolindad@yahoo.com.br>
2024-02-06 21:31:58 -0300

    Add image

--- services/supabase.js
+++ services/supabase.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'SUA URL'
@@ -5,4 +6,28 @@ const supabaseKey = 'SUA CHAVE'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-export { supabase }
+async function uploadImage(file) {
+    const { data, error } = await supabase.storage
+      .from('imagens')
+      .upload(`${uuidv4()}-${file.name}`, file)
+
+    if (error) throw new Error('Error uploading image:', error.message);
+
+    return data;
+}
+
+async function getImage(path) {
+    const { data, error } = await supabase.storage
+      .from('feed-images')
+      .list(path);
+
+    if (error) throw new Error('Error getting images:', error.message);
+
+    return data;
+}
+
+export {
+    supabase,
+    uploadImage,
+    getImage
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { supabase } from '../services/supabase.js'
+import { supabase, uploadImage, getImage  } from '../services/supabase.js'
 import { useState, useEffect } from 'react'
 
 import Login from './Login'
 
 function App() {
+  const [file, setfile] = useState([]);
+  const [images, setImages] = useState([]);
   const [session, setSession] = useState(null)
 
   useEffect(() => {
@@ -20,14 +22,38 @@ function App() {
     return () => subscription.unsubscribe()
   }, [])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const newImage = await uploadImage(file);
+      setImages((prev) => [...prev, newImage]);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleFileSelected = (e) => {
+    setfile(e.target.files[0]);
+  };
+
   if (!session) {
     return <Login />
   } 
 
   return (
-    <form>
-      <input type="file" name="image" />
+    <form onSubmit={handleSubmit}>
+      <input type="file" name="image" onChange={handleFileSelected} />
       <button type="submit">Upload image</button>
+      <div>
+        {images.map((image) => (
+          <img
+            key={image.Key}
+            src={image.url}
+            alt={image.Key}
+            style={{ width: 200 }}
+          />
+        ))}
+      </div>
     </form>
   );
 }
