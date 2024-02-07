c3258c74ded61fab87da79fea8c0e6ea0aac117a
José Olinda <joseolindad@yahoo.com.br>
2024-02-06 21:00:29 -0300

    uPDATE

--- services/supabase.js
+++ services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tthfkmzhijkauchymzsn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0aGZrbXpoaWprYXVjaHltenNuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzI1NTA2MywiZXhwIjoyMDIyODMxMDYzfQ.rdVqF6p1bLPKWMNbOsATU9tkwaDdBcVeq6khFr0glk4'
+const supabaseUrl = 'SUA URL'
+const supabaseKey = 'SUA CHAVE'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
